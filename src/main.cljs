bbc27255668df7ed72ae4211dc9f614bb73b0e61
Mike Zamansky <zamansky@gmail.com>
2019-11-27 09:08:06 -0500

    got local state (rum) example working)

--- src/main.cljs
+++ src/main.cljs
@@ -1,13 +1,26 @@
 (ns main
-  (:require [reagent.core :as r]
-            [cljs.core.async :refer (chan put! <! go go-loop timeout)]
+  (:require [cljs.core.async :refer (chan put! <! go go-loop timeout)]
             [rum.core :as rum]
             ))
 
+(rum/defcs stateful < (rum/local {:username "un" :password "pw"} :cred)
+  [state]
+  (let [creds (:cred state)]
+    [:div  "applesauce: " (:password @creds)]))
 
+
+(rum/defc main-component []
+  [:div
+   [:div.p-5 "Hello World"]
+   (stateful )
+
+   ]
+  )
 (defn reload! []
-  (print "Hello reload!"))
+(rum/mount (main-component) (.getElementById js/document "app"))
+(print "Hello reload!"))
 
 (defn main! []
+(rum/mount (main-component) (.getElementById js/document "app"))
 (print "Hello main!")
 )
