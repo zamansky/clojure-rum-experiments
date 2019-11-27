bfbf5ff2330db5553c5cbc055d101c24be31f905
Mike Zamansky <zamansky@gmail.com>
2019-11-27 15:35:15 -0500

    changed sate var to localstate so we can easily make a state ns

--- src/main.cljs
+++ src/main.cljs
@@ -6,40 +6,37 @@
 
 
 
-(defn login[state]
-  (print state)
+(defn login[local-state]
+  (print local-state)
   )
 
-(defn logout[state]
-  (print state)
+(defn logout[local-state]
+  (print local-state)
   )
 
-(defn login-enter[e state]
+(defn login-enter[e local-state]
   (if (= "Enter" (.-key e))
-    (login state)
+    (login local-state)
     )
   )
 
 
-(rum/defc input [type name state field]
+(rum/defc input [type name local-state field]
   [:div
    [:label.text-tray-500.font-bold.md:text-right.mx-4.py-1 (str name ": ")]
    [:input.appearance-none.h-1.bg-gray-200.border-2.border-gray200.rounded.px-2.py-4
     {:type type
-     :value (field @state)
-     :on-change #(swap!  state assoc field  (-> % .-target .-value))
-     on-key-up #(login-enter % state)
-     }
-    ]
-   ]
-  )
+     :value (field @local-state)
+     :on-change #(swap!  local-state assoc field  (-> % .-target .-value))
+     on-key-up #(login-enter % local-state)
+     }]])
 
-(rum/defcs login-form < (rum/local {:username "un" :password "pw"} :cred)
-  [state]
-  (let [creds (:cred state)]
+(rum/defcs login-form < (rum/local {:username "" :password ""} :cred)
+  [local-state]
+  (let [creds (:cred local-state)]
     [:div.flex
      (input "text" "username" creds :username)
-     (input "text" "password" creds :password)
+     (input "password" "password" creds :password)
      [:button.bg-blue-500.hover:bg-blue-700.text-white.font-bold.px-3..mx-4.my-1.rounded {:on-click #(login creds) :id "login"}"Login"]
      ]
     
